40662d14b158d060091b2176da1c6bca3de4fa1f
Duve, Michael <mduve@designmail.net>
2016-07-13 22:18:10 +0200

    quality updated

--- imageslicer.py
+++ imageslicer.py
@@ -89,7 +89,7 @@ def create_thumb(img, data, path, current_level):
     outfile = "map_thumb_"+str(current_level)+".jpg"
     thumb_img = img
     thumb_img.thumbnail((settings.thumbSize, settings.thumbSize), Image.ANTIALIAS)
-    thumb_img.save(settings.output + path + outfile, optimize=True)
+    thumb_img.save(settings.output + path + outfile, optimize=True, quality=75)
     data["thumb"] = path + outfile
 
 
@@ -183,8 +183,7 @@ def calculate_slices_normal(width, height, size, x_remainder, y_remainder, small
 
 def crop_save(image, box, x, y, current_level):
     cropped_img = image.crop(box)
-    cropped_img.save(settings.output + settings.path + "map_"+str(current_level)+"_"+str(y)+"_"+str(x)+"."+settings.extension, optimize=True)
-
+    cropped_img.save(settings.output + settings.path + "map_"+str(current_level)+"_"+str(y)+"_"+str(x)+"."+settings.extension, quality=95, optimize=True)
 
 def open_image(file_name):
     try:
